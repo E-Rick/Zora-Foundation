b0b6ceaae2067ef324bb1a0b05a8e02555a3c86a
Erick Martinez <erick@wrecs.studio>
2022-06-08 13:45:36 -0400

    add(tw): tailwind and postcss configs

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"@tailwindcss/jit": {},
+	autoprefixer: {},
+	content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
