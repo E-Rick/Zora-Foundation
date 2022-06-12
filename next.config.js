f9b8bf6cef7bdaa7baafca5093e1a56dc6b3fa70
Erick Martinez <erick@wrecs.studio>
2022-06-12 16:02:17 -0400

    add(pkg): @fontsource ia-writer-mono and inter
    adds framer-motion and styled components too

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	images: {
+		domains: ['images.mirror-media.xyz'],
+	},
 }
 
 module.exports = nextConfig
