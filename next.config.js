1847d80249a3b7c758022ba24f260a88bc15541e
Erick Martinez <erick@wrecs.studio>
2022-06-13 12:36:17 -0400

    build: add new image domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['images.mirror-media.xyz', 'gateway.pinata.cloud'],
+		domains: ['images.mirror-media.xyz', 'gateway.pinata.cloud', 'f8n-production-collection-assets.imgix.net'],
 	},
 }
 
